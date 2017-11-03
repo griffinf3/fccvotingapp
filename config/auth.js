52b255c6844f723b8f6a229962afd832c9b84558
Franklin Griffin <griffinf3@yahoo.com>
2017-11-03 18:19:03 -0400

    My first commit

--- config/auth.js
+++ config/auth.js
@@ -3,15 +3,15 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     'twitterAuth' : {
-        'consumerKey'       : 'B6eaJOOiQ1srl77PAaTngY0PJ',
-        'consumerSecret'    : 'GKDIW8vWszHFbgtE30A5ec9zG5DGdZ10UO6PiYfmFsOEodSRDi',
-        'callbackURL'        : 'http://localhost:3000/auth/twitter/callback'
+        'consumerKey'    : 'B6eaJOOiQ1srl77PAaTngY0PJ',
+        'consumerSecret' : 'GKDIW8vWszHFbgtE30A5ec9zG5DGdZ10UO6PiYfmFsOEodSRDi',
+        'callbackURL'    : 'https://postnpoll.herokuapp.com/auth/twitter/callback'
     },
     
    'googleAuth' : {
         'clientID'      : '762644857277-di7slrft4cvqdthb5kr4b5cg33gem016.apps.googleusercontent.com',
         'clientSecret'  : '__VRk0AkEgtU6-ERLYyazHPx',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://postnpoll.herokuapp.com/auth/google/callback'
     }  
     
 };
