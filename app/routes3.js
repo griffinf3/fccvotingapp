e9ffd0c37f0fe5d9e12063c1397c119ccdaee078
Franklin Griffin <griffinf3@yahoo.com>
2017-11-04 18:36:55 -0400

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -292,28 +292,15 @@ app.post('/voting', function(req, res) {
                    
                          
                      }
-                    //res.redirect('/'); 
+                     res.redirect('/'); 
 
                      
             }
-            else {
-                
-                
-                //res.redirect('/'); 
-            
-            
-            }}
+            else {res.redirect('/'); }}
     
-    if (req.user)
-    {res.redirect('/'); }
-    else {
-        
-  
-   }
+    
  
 );
-    
-    
 });
      
 app.get('/delete/*', function(req, res) {
