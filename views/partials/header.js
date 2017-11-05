d7a47081e9e6cd973c15ec6e97df507daccf558e
Franklin Griffin <griffinf3@yahoo.com>
2017-11-05 14:51:06 -0500

    My first commit

---
+++ views/partials/header.js
@@ -0,0 +1,11 @@
+<div class="header">
+    <div class="container text-center">
+<div id="siteName" style="display: inline">Post-n-Poll</div>
+<div style="display: inline">&copy;</div>
+</div>
+    
+    
+    <div id="description"><p>(Set up an account for free, log in, post questions, and view participant responses)</p><a href="https://www.freecodecamp.org/griffinf3">
+  <img src="fcc.jgp," alt="FreeCodeCamp profile" style="width:42px;height:42px;border:0;">
+</a></div>
+  </div>
