338516be704fd9255efc1a2a3887d6cf934a7240
Franklin Griffin <griffinf3@yahoo.com>
2017-11-06 16:44:54 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -598,7 +598,7 @@ app.get('/loginSuccess', function(req, res, next) {
                             if (err) {}
                             else
                               { if (doc) {
-                                  res.send('OK'+ id);
+                                  res.send('OK');
                                                 }
                            }
                           });
