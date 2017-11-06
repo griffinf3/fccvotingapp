f0cb80be286eb665505cf7e3e26d281d4779d6d8
Franklin Griffin <griffinf3@yahoo.com>
2017-11-06 09:25:37 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -2,20 +2,23 @@ var User       = require('../app/models/user');
 var Poll       = require('../app/models/poll');
 var Option     = require('../app/models/option');
 
+
+
+module.exports = function(app, passport) {
+
 function allPolls(){
 var allPolls = [{}];
                     allPolls[0] = {question: 'poll 1', options: [{}, {option: 'option 1', votes: 0}, {option: 'option 2', votes: 0}, {option: 'option 3', votes: 0}]};
                     allPolls[1] =  {question: 'poll 2', options: [{},{option: 'option 1', votes: 0}, {option: 'option 2', votes: 0}, {option: 'option 3', votes: 0}]};
                     allPolls[2] =  {question: 'poll 3', options: [{},{option: 'option 1', votes: 0}, {option: 'option 2', votes: 0}, {option: 'option 3', votes: 0}]};
- return allPolls[0];     
+ return allPolls;     
 }
-
-module.exports = function(app, passport) {
+    
     
 // normal routes ===============================================================
 app.get('/', isLoggedIn, function(req, res) {
 var allPolls = [];
-// allPolls[0] = allPolls();
+allPolls = allPolls();
  res.send('OK');}
      
        );
