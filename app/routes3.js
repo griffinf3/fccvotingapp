84104d0418a80f5252a9abc81fb4f660e58e69e9
Franklin Griffin <griffinf3@yahoo.com>
2017-11-07 13:33:40 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -214,17 +214,7 @@ app.get('/view', isLoggedIn, function(req, res) {
     });
     
 function findDelete(req, res, next) {
-    var id = req.user._id;      
-    var question = req.body.question;
-    Poll.findOneAndRemove({'userid' : id, 'poll.question' : question}, function (err, doc, next) {
-    if(err)throw err;
-	else if (!doc) {
-    Poll.findOneAndRemove({'userid' : id, 'poll.question' : question+ '?'}, function (err, doc, next) {
-    if(err)throw err; else {return next();}});    
-    }
-    else return next();    
-    });
-    
+    return next();
 }
     
 app.post('/create', {isLoggedIn2, findDelete}, function(req, res) { 
