6ae2c73e9ae57a8a7f564a2278bd29097a49d7d3
Franklin Griffin <griffinf3@yahoo.com>
2017-11-07 11:49:07 -0500

    My first commit

--- app/models/poll.js
+++ app/models/poll.js
@@ -11,8 +11,7 @@ var pollSchema = mongoose.Schema({
   poll: {
     question:  String,
     showcase:  Boolean,
-    options: [{}]
-      ///: [{ option: String, votes: Number }]
+    options: [{}] 
   }
 });
 
