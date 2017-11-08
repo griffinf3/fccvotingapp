d30f4f463cfae4fad0c8b61e9f13b5ee16937f12
Franklin Griffin <griffinf3@yahoo.com>
2017-11-08 12:25:58 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -183,7 +183,7 @@ app.get('/view', isLoggedIn, function(req, res) {
                          
                       }});}} 
                             //res.render('view.ejs', {qnamelist: qnamelist, type: type});
-                            res.send(lg);
+                            res.send('public');
                         }}});}
                     else
                     {Poll.find({ 'userid' : id}, function(err, doc) {    
