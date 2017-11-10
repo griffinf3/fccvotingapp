0a6518e27ced989604cc9105073427a36d628f34
Franklin Griffin <griffinf3@yahoo.com>
2017-11-10 13:14:57 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -237,30 +237,9 @@ app.get('/view2', function(req, res) {
                           var pusername;
                           var namelist = [];
                           var nameobj = {};
-                          User.find({},function(err, puser) { 
-                                 if (err) {} else { 
-                                    for (var i = 0; i< puser.length; i++)  
-                                    {pusername = puser[i].local.username;
-                                     nameobj = {id: puser[i]._id, username: pusername};
-                                     namelist.push(nameobj);} 
-                                     callback("",namelist);
-                                    }});
-                             function callback (error, list){    
-                              var lg = doc.length;
-                              var qnamelist = [];
-                              var qnameobj = {};
-                              var pid;
-                              var pq;
-                              var nobj;
-                             for (var i=0; i<lg; i++)
-                             {pid= doc[i].userid;
-                              pq = doc[i].poll.question;
-                              nobj = list.filter(function (list) {return list.id == pid });
-                              qnameobj = {username: nobj[0].username, question: pq};
-                              qnamelist.push(qnameobj);
-                             } res.send('OK');
+                          res.send('OK');
 //res.render('view.ejs', {questionlist: [], qnamelist: qnamelist, username: '', viewtype: "public", logstatus: ' Login/Signup',totalPolls:0});   
-                            } }
+                            } 
                          else { res.send('no doc');}
                         } });
 });
