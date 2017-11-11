5b6dba9880c43f4f3d81930e0982b7c2081d0f54
Franklin Griffin <griffinf3@yahoo.com>
2017-11-11 07:06:16 -0500

    My first commit

--- app/routes3.js
+++ app/routes3.js
@@ -327,23 +327,8 @@ return next();
 app.post('/create', findDelete, function(req, res) { 
     var id = req.user._id;      
     var question = req.body.question;
-    var options = [{}]; 
-  for (i=0; i<req.body.options.length; i++)
-  {options.push({option: req.body.options[i], votes: 0});}
-    var showCase =  req.body.showcase;
-    var SC= false;
-    if (showCase =='showcase') SC = true; 
-    var makePublic = req.body.visibility;
-    var mP = false;
-    if (makePublic =='public') mP = true; 
-// create new poll document for the user.  
-var newPollUser = new Poll({userid: req.user._id, poll: {question: question, showcase: SC,options: options, public: mP}}); 
-                       
-// save  
-newPollUser.save(function(err) {
-                              if (err) throw err;
-res.redirect('/');
    
+  res.send(question);
 });
   
     
